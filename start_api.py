e0992d1641235ef1b8a0ddfcc8f8171914aa2346
Vidit-01 <viditanupgupta@gmail.com>
2026-04-21 18:23:14 +0530

    fix

--- start_api.py
+++ start_api.py
@@ -17,7 +17,7 @@ import uvicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     uvicorn.run(
-        "backend.main:app",
+        "main:app",
         host="0.0.0.0",
         port=port,
         reload=True,
